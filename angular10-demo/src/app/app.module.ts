5bf4501522021a5bec1d7abcf56dd211f8cb77f1
MyName <you@gmail.com>
2022-08-10 17:51:44 +0530

    new ang10 project created with 2 fresh modules

--- angular10-demo/src/app/app.module.ts
+++ angular10-demo/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    AboutusComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule
