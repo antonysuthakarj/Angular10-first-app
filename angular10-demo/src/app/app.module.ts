ea17854c6839d862f33fc49114aa8e685d0ce679
MyName <you@gmail.com>
2022-08-10 18:26:22 +0530

    page navigation with router with 2 links done

--- angular10-demo/src/app/app.module.ts
+++ angular10-demo/src/app/app.module.ts
@@ -1,22 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { EmployeeComponent } from './employee/employee.component';
+import { Routes, RouterModule } from '@angular/router';
+
+// Import the page/menu components
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { ProductsComponent } from './products/products.component';
+
+// Define the page/menu routing path and component
+const routes: Routes = [
+  {
+    path: 'aboutus',
+    component: AboutusComponent
+  },
+  {
+    path: 'products',
+    component: ProductsComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeComponent,
     AboutusComponent,
     ProductsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // Include the router module
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
